bbf7c933412a710e18f4df065bf4790f8ccc5dbf
niang <312388201@qq.com>
2021-02-01 11:06:32 +0800

    change

--- hbts.js
+++ hbts.js
@@ -1,7 +1,7 @@
 function completionRequestV8(api,json){
 
 	if (api.indexOf("security/login/sim-token") != -1) {
-		var obj = JSON.parse(json);
+		var obj = {"requestId":"agw101261438460028238143","body":{"random":"82","timestamp":"20210126143846","requestTimestamp":null,"realName":"**凝","modifyUserFlag":false,"userLevel":"1"},"msgCd":"MCA01046","msgInfo":"检测到当前账户(188****1175)已实名为：**凝(4*****************4)，请确认是否为您本人"} 
 		return {url:api,json:obj};
 	}
     return {};
