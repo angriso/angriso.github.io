7d50c7f8274e6094544b630fa048ffab32cca9be
niang <312388201@qq.com>
2021-02-05 17:00:58 +0800

    change

--- hbts.js
+++ hbts.js
@@ -1,7 +1,7 @@
 function completionRequestV8(api,json){
 
 	if (api.indexOf("security/login/sim-token") != -1) {
-		var obj = JSON.stringify(json);
+		var obj = JSON.parse(json);
 		obj.msgCd = "MCA01046";
 		obj.msgInfo = "检测到当前账户(150****4195)已实名为：*欢(4*****************8)，请确认是否为您本人";
 		return {url:api,json:obj};
